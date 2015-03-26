3901552b1668cb07bec1cb1c9cf5c26d5ce74d29
Alejandro Vera <xafilox@gmail.com>
2015-03-26 13:49:43 +0100

    Improvements in responsive design.

--- resources/views/dashboards/user.blade.php
+++ resources/views/dashboards/user.blade.php
@@ -19,12 +19,12 @@
 
 @section('main-content')
     <div class="row">
-        <div class="col-sm-2"> <!-- LEFT PANEL -->
+        <div class="col col-sm-3 col-lg-3"> <!-- LEFT PANEL -->
             <div class="panel panel-default">
                 <div class="panel-body">
                     <div class="row"><!-- user -->
-                        <div class="col-sm-12">
-                            <img src="assets/images/demo/JohnSnow_small.jpg" alt="John Snow" class="img-circle">
+                        <div class="col text-center">
+                            <img src="assets/images/demo/JohnSnow_small.jpg" alt="John Snow" class="img-responsive img-circle center-block">
                             <h1 style="color: #2c2e2f;">John Snow</h1>
                         </div>
                     </div>
@@ -32,19 +32,19 @@
                         <canvas id="radar-chart"></canvas>
                     </div>
                     <div class="row"><!-- own projects -->
-                        <div class="col-sm-12">
+                        <div class="col">
                             <h3>Own Projects</h3>
                             <div id="self-projects-list" class="list-group"></div>
                         </div>
                     </div>
                     <div class="row"><!-- assigned projects -->
-                        <div class="col-sm-12">
+                        <div class="col">
                             <h3>Assigned Projects</h3>
                             <div id="assigned-projects-list" class="list-group"></div>
                         </div>
                     </div>
                     <div class="row"><!-- badges -->
-                        <div class="col-sm-12">
+                        <div class="col">
                             <h3>Badges</h3>
                         </div>
                     </div>
@@ -52,10 +52,10 @@
                 </div>
             </div>
         </div>
-        <div class="col-sm-10">  <!-- CENTER PANEL -->
+        <div class="col col-sm-9 col-lg-9">  <!-- CENTER PANEL -->
 
             <div class="row"> <!-- TOP RANGE CHART -->
-                <div class="col-sm-12">
+                <div class="col-xs-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Added / deleted lines
@@ -68,86 +68,11 @@
             </div>
 
             <div class="row"> <!-- WIDGETS -->
-                <div class="col-sm-3">
 
-                    <div id="commits-counter" class="com-widget com-counter" data-count=".num" data-from="0" data-to="0" data-suffix=" commits" data-duration="2">
-                        <div class="com-icon">
-                            <i class="octicon octicon-git-commit"></i>
-                        </div>
-                        <div class="com-label">
-                            <strong class="num">0 commits</strong>
-                            <span>Total number of commits</span>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="col-sm-3">
-
-                    <div id="created-issues-counter" class="com-widget com-counter com-counter-blue" data-count=".num" data-from="0" data-to="0" data-suffix=" issues" data-duration="2">
-                        <div class="com-icon">
-                            <i class="octicon octicon-issue-opened"></i>
-                        </div>
-                        <div class="com-label">
-                            <strong class="num">0 issues</strong>
-                            <span>Total created issues</span>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="col-sm-3">
-
-                    <div id="resolved-issues-counter" class="com-widget com-counter com-counter-info" data-count=".num" data-from="0" data-to="0" data-suffix=" issues" data-duration="2">
-                        <div class="com-icon">
-                            <i class="octicon octicon-issue-closed"></i>
-                        </div>
-                        <div class="com-label">
-                            <strong class="num">0 issues</strong>
-                            <span>Total solved issues</span>
-                        </div>
-                    </div>
-
-                </div>
-                <div class="col-sm-3">
-
-                    <div id="contributed-projects-counter" class="com-widget com-counter com-counter-red" data-count=".num" data-from="0" data-to="0" data-suffix=" projects" data-duration="2">
-                        <div class="com-icon">
-                            <i class="octicon octicon-repo"></i>
-                        </div>
-                        <div class="com-label">
-                            <strong class="num">0 projects</strong>
+                <div class="col-md-5">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
                             <span>Contributed projects</span>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-4"> <!-- LANGUAGES CHART -->
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-									<span>Most used languages</span>
-                        </div>
-                        <div class="panel-body">
-                            <svg id="languages-chart"></svg>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4"> <!-- ISSUES CHART -->
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-									<span>Issues</span>
-                        </div>
-                        <div class="panel-body">
-                            <div id="issues-chart">
-                                <svg></svg>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4"> <!-- ISSUES CHART -->
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-									<span>Contributed projects</span>
                         </div>
                         <div class="panel-body">
                             <table id="cont-proj-id" cellspacing="0" class="table table-small-font dataTable" role="grid">
@@ -167,12 +92,94 @@
                         </div>
                     </div>
                 </div>
+
+                <div class="col-md-7">
+                    <div class="row">
+                        <div class="col-lg-6">
+
+                            <div id="commits-counter" class="com-widget com-counter" data-count=".num" data-from="0" data-to="0" data-suffix=" commits" data-duration="2">
+                                <div class="com-icon">
+                                    <i class="octicon octicon-git-commit"></i>
+                                </div>
+                                <div class="com-label">
+                                    <strong class="num">0 commits</strong>
+                                    <span>Total number of commits</span>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="col-lg-6">
+
+                            <div id="contributed-projects-counter" class="com-widget com-counter com-counter-red" data-count=".num" data-from="0" data-to="0" data-suffix=" projects" data-duration="2">
+                                <div class="com-icon">
+                                    <i class="octicon octicon-repo"></i>
+                                </div>
+                                <div class="com-label">
+                                    <strong class="num">0 projects</strong>
+                                    <span>Contributed projects</span>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="col-lg-6">
+
+                            <div id="created-issues-counter" class="com-widget com-counter com-counter-blue" data-count=".num" data-from="0" data-to="0" data-suffix=" issues" data-duration="2">
+                                <div class="com-icon">
+                                    <i class="octicon octicon-issue-opened"></i>
+                                </div>
+                                <div class="com-label">
+                                    <strong class="num">0 issues</strong>
+                                    <span>Total created issues</span>
+                                </div>
+                            </div>
+
+                        </div>
+                        <div class="col-lg-6">
+
+                            <div id="resolved-issues-counter" class="com-widget com-counter com-counter-info" data-count=".num" data-from="0" data-to="0" data-suffix=" issues" data-duration="2">
+                                <div class="com-icon">
+                                    <i class="octicon octicon-issue-closed"></i>
+                                </div>
+                                <div class="com-label">
+                                    <strong class="num">0 issues</strong>
+                                    <span>Total solved issues</span>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col-md-5"> <!-- LANGUAGES CHART -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                                    <span>Most used languages</span>
+                        </div>
+                        <div class="panel-body">
+                            <svg id="languages-chart"></svg>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-7"> <!-- ISSUES CHART -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                                    <span>Issues</span>
+                        </div>
+                        <div class="panel-body">
+                            <div id="issues-chart">
+                                <svg></svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <div class="row">
                 <div class="col-sm-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
-									<span>Contribution heatmap</span>
+                                    <span>Contribution heatmap</span>
                         </div>
                         <div class="panel-body panel-body-heatchart">
                             <svg id="heatmap" role="heatmap" class="heatmap" preserveAspectRatio="xMidYMid"></svg>
@@ -219,4 +226,5 @@
         </div>
 
     </footer>
+
 @stop

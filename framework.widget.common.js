e2063cb5d6b6f848ea77486d6e9fed5de97bf8bc
Alejandro Vera <xafilox@gmail.com>
2015-05-12 12:49:44 +0200

    [Widget] [Common] Blur when loading.

--- framework.widget.common.js
+++ framework.widget.common.js
@@ -6,42 +6,45 @@
         return;
     }
 
-	var CommonWidget = function CommonWidget(extending) {
+
+	var CommonWidget = function CommonWidget(extending, container) {
 		if (extending === true) {
             return;
         }
-		this.loadingContainer = document.createElement('div');
-        this.loadingContainer.className ='loadingContainer';
+        this._common = {};
+        this._common.container = container;
+        this._common.loadingContainer = document.createElement('div');
+        this._common.loadingContainer.className ='loadingContainer';
         var loadingLayer = document.createElement('div');
         loadingLayer.className ='loadingLayer off';
         var spinner = document.createElement('i');
         spinner.className ='fa fa-spinner fa-pulse';
 
-        this.loadingContainer.appendChild(loadingLayer);
+        this._common.loadingContainer.appendChild(loadingLayer);
         loadingLayer.appendChild(spinner);
-		this.container.parentElement.appendChild(this.loadingContainer);
-        this.loadingLayer = loadingLayer;
+        this._common.container.appendChild(this._common.loadingContainer);
+        this._common.loadingLayer = loadingLayer;
 	};
 
     var oldContainerClass;
 
     CommonWidget.prototype.startLoading = function() {
-        if (this.loadingLayer.className == 'loadingLayer off') {
-            var wsize = this.container.getBoundingClientRect();
-            this.loadingLayer.style.lineHeight = wsize.height + 'px';
-            this.loadingContainer.style.height = wsize.height + 'px';
-            this.loadingContainer.style.width = wsize.width + 'px';
-            //this.loadingContainer.style.top = wsize.top + 'px';// Not necesary with bootstrap
-            this.loadingContainer.style.lineHeight = wsize.left + 'px';
-            oldContainerClass = this.container.className;
-            this.container.className = oldContainerClass +' blurMode';
-        	this.loadingLayer.className ='loadingLayer on';
+        if (this._common.loadingLayer.className == 'loadingLayer off') {
+            var wsize = this._common.container.getBoundingClientRect();
+            this._common.loadingLayer.style.lineHeight = wsize.height + 'px';
+            this._common.loadingContainer.style.height = wsize.height + 'px';
+            this._common.loadingContainer.style.width = wsize.width + 'px';
+            //this._common.loadingContainer.style.top = wsize.top + 'px';// Not necesary with bootstrap
+            this._common.loadingContainer.style.lineHeight = wsize.left + 'px';
+            oldContainerClass = this._common.container.className;
+            this._common.container.className = oldContainerClass +' blurMode';
+            this._common.loadingLayer.className ='loadingLayer on';
         }
     };
 
     CommonWidget.prototype.endLoading = function() {
-        this.container.className = oldContainerClass;
-		this.loadingLayer.className ='loadingLayer off';
+        this._common.container.className = oldContainerClass;
+        this._common.loadingLayer.className ='loadingLayer off';
 
     };
 

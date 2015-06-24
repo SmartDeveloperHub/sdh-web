f2d2f5a66191b08a39fc7ea1848c1c1573173d1e
Carlos Blanco <cblanco@conwet.com>
2015-06-24 10:51:27 +0200

    Improve light theme

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -10,7 +10,7 @@
         @yield('css')
         <script data-main="assets/js/dashboardLoader" src="assets/js/requirejs/require.js"></script>
     </head>
-    <body class="page-body light">
+    <body class="page-body">
     @yield('header')
     @yield('body')
     @yield('scripts')

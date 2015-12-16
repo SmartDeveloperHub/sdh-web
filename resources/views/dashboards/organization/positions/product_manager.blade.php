0f3bd0fa6277a39425804aceb0e373fe3bb4879e
Alejandro Vera <xafilox@gmail.com>
2015-12-16 09:11:16 +0100

    Missing tooltips.

--- resources/views/dashboards/organization/positions/product_manager.blade.php
+++ resources/views/dashboards/organization/positions/product_manager.blade.php
@@ -178,9 +178,9 @@
         <!-- Subsection: Developers/externals -->
         <div class="grid-stack-item" data-gs-width="6" data-gs-height="3" data-gs-x="0" data-gs-y="5">
             <div id="external-members-subtitle" class="grid-stack-item-content subtitleRow">
-                <span id="external-members-table-stitle-ico" class="subtitleIcon fa fa-user-secret"></span>
-                <span id="external-members-table-stitle-label" class="subtitleLabel">External developers</span>
-                <span id="external-members-table-stitle-help" class="subtitleHelp fa fa-info-circle"></span>
+                <span id="external-members-stitle-ico" class="subtitleIcon fa fa-user-secret"></span>
+                <span id="external-members-stitle-label" class="subtitleLabel">External developers</span>
+                <span id="external-members-stitle-help" class="subtitleHelp fa fa-info-circle"></span>
             </div>
         </div>
         <div class="grid-stack-item" data-gs-width="6" data-gs-height="10" data-gs-x="0" data-gs-y="8">
@@ -189,9 +189,9 @@
 
         <div class="grid-stack-item" data-gs-width="6" data-gs-height="3" data-gs-x="6" data-gs-y="5">
             <div id="internal-members-subtitle" class="grid-stack-item-content subtitleRow">
-                <span id="internal-members-table-stitle-ico" class="subtitleIcon fa fa-user"></span>
-                <span id="internal-members-table-stitle-label" class="subtitleLabel">Internal developers</span>
-                <span id="internal-members-table-stitle-help" class="subtitleHelp fa fa-info-circle"></span>
+                <span id="internal-members-stitle-ico" class="subtitleIcon fa fa-user"></span>
+                <span id="internal-members-stitle-label" class="subtitleLabel">Internal developers</span>
+                <span id="internal-members-stitle-help" class="subtitleHelp fa fa-info-circle"></span>
             </div>
         </div>
         <div class="grid-stack-item" data-gs-width="6" data-gs-height="10" data-gs-x="6" data-gs-y="8">
@@ -317,7 +317,7 @@
         var positionLine = '<div><span class="toolTitle"><p>Members by positions.</p></span></div><div><span class="toolRow">This chart analyzes the distribution of members by position during the selected period of time.</span></div>';
         addQTip($('#roles-stitle-help'), "positionsLineTool", positionLine);
         // Manager Selector
-        var teamSelect = '<div><span class="toolTitle"><p>Most significant Teams.</p></span></div><div><span class="toolRow">Select for comparison.</span></div>';
+        var teamSelect = '<div><span class="toolTitle"><p>Most significant Projects.</p></span></div><div><span class="toolRow">Select for comparison.</span></div>';
         addQTip($('#members-table-stitle-help'), "teamSelectTool", teamSelect);
         // Manager Comparison
         var managerComp = '<div><span class="toolTitle"><p>Manager team roles comparison.</p></span></div><div><span class="toolRow">This chart compares the number of team members per role and manager.</span></div>';
@@ -325,6 +325,13 @@
         // Total Member Roles
         var memberRoles = '<div><span class="toolTitle"><p>Total members by role.</p></span></div><div><span class="toolRow">Accumulated number of team members by role.</span></div>';
         addQTip($('#team-pie-stitle-help'), "memberRolesTool", memberRoles);
+        //External members
+        var externalMembers = '<div><span class="toolTitle"><p>External developers</p></span></div><div><span class="toolRow">Number of external developers per product</span></div>';
+        addQTip($('#external-members-stitle-help'), "externalMembers", memberRoles);
+        //Internal members
+        var internalMembers = '<div><span class="toolTitle"><p>Internal developers</p></span></div><div><span class="toolRow">Number of internal developers per product</span></div>';
+        addQTip($('#internal-members-stitle-help'), "internalMembers", memberRoles);
+
 
         var env = framework.dashboard.getEnv();
         framework.data.updateContext(orgCtx, {oid: env['oid']});
